99f6bbcc6c1fbae04d6f6779b19695621a9d2133
CodeQuiver <38484031+CodeQuiver@users.noreply.github.com>
2018-09-26 18:40:44 -0400

    set up mongoose connection to database and tested successfully

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //Require modules
 var express = require("express");
-var exphbs = require("express-handlebars");
+var mongoose = require("mongoose");
+var expressHandlebars = require("express-handlebars");
 var bodyParser = require("body-parser");
 
 //Port setup- either Heroku port/whatever designated port by the process.env file
@@ -15,7 +16,7 @@ var router = express.Router(); //set up router
 app.use(express.static(__dirname + "public"));
 
 // Connect Handlebars and express
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.engine("handlebars", expressHandlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 //BODY-PARSER STANDARD SETUP WITH EXPRESS
@@ -25,6 +26,20 @@ app.use(bodyParser.json());
 //Send requests through Router middleware
 app.use(router);
 
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var db = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+//Connect mongoose to database defined above
+mongoose.connect(db, function (error) {
+  //if there are any errors connecting, log them
+  if (error) {
+    console.log(error);
+  } 
+  //else log success connecting message
+  else {
+    console.log("mongoose successfully connected to database");  
+  }
+});
 
 // START LISTENER - begin listening to client requests
 app.listen(PORT, function() {
