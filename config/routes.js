67aa9aca10b7bf286ff4de803eb22c71ca9f634c
CodeQuiver <38484031+CodeQuiver@users.noreply.github.com>
2018-09-28 15:44:41 -0400

    simplified code and re-setup to try to fix static directory issue but so far still having same error. Attempted a few experimental fixes in server.js, they are currently commented out as they caused some change but didn't solve the prblem

--- config/routes.js
+++ config/routes.js
@@ -1,12 +1,12 @@
-module.exports = function (router) {
+module.exports = function(router) {
     //render the homepage
-    router.get("/", function (req,res) {
+    router.get("/", function(req,res) {
         res.render("home");
-        //knows home means home.handlebars because handlebars was defined as the view engine
+        //knows home means home.handlebars because handlebars was defined as the view engine- or at least it should. Having technical issues.
     });
 
     //render the "saved" handlebars page
-    router.get("/saved", function (req,res) {
+    router.get("/saved", function(req,res) {
         res.render("saved");
     });
 }
--- server.js
+++ server.js
@@ -4,41 +4,49 @@ var mongoose = require("mongoose");
 var expressHandlebars = require("express-handlebars");
 var bodyParser = require("body-parser");
 
+
+
 //Port setup- either Heroku port/whatever designated port by the process.env file
 //or port 3000 for local setups with no .env file
 var PORT = process.env.PORT || 3000;
 
 //EXPRESS SETUP
-var app = express(); //instantiate app
-var router = express.Router(); //set up router
 
-//import routes file and pass it the express router set up above as an argument
-require("./config/routes")(router);
+//instantiate express app
+var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(__dirname + "/public"));
+// app.use(express.static("public")); // experiment
+// app.set('views', __dirname + '\\public\\views'); // experiment
+
+//set up express router
+var router = express.Router();
+
+// Require our routes file and pass it the router object as an argument
+require("./config/routes")(router);
 
 
-// Connect Handlebars and express
+//Connect Handlebars to express app
 app.engine("handlebars", expressHandlebars({
-  defaultLayout: "main" 
+  defaultLayout: "main"
 }));
 app.set("view engine", "handlebars");
 
 //BODY-PARSER STANDARD SETUP WITH EXPRESS
 app.use(bodyParser.urlencoded({
-  extended: false
+  extended: true
 }));
-// app.use(bodyParser.json());
+app.use(bodyParser.json());
 
 //Send requests through Router middleware
 app.use(router);
 
-// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+// If deployed, use the deployed database, otherwise use the local mongoHeadlines database
 var db = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 //Connect mongoose to database defined above
-mongoose.connect(db, function (error) {
+mongoose.connect(db, function(error) {
   //if there are any errors connecting, log them
   if (error) {
     console.log(error);
@@ -52,5 +60,9 @@ mongoose.connect(db, function (error) {
 // START LISTENER - begin listening to client requests
 app.listen(PORT, function() {
     // Log (server-side) when our server has started
-    console.log("Server listening on: http://localhost:" + PORT);
+    console.log("Server listening on port: " + PORT);
   });
+
+
+
+
