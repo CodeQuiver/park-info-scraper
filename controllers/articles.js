ef25697ba8fc8c3029e8bfa42b5327d677fb8acd
CodeQuiver <38484031+CodeQuiver@users.noreply.github.com>
2018-09-29 18:59:47 -0400

    set up CRUD functionality for articles

--- controllers/articles.js
+++ controllers/articles.js
@@ -7,7 +7,8 @@ var Article = require("../models/ArticleModel");
 var Comment = require("../models/CommentModel");
 
 module.exports = {
-    fetch: function (callback) {
+    //fetch = Create CRUD function
+    fetch: function(callback) {
         scrape(function(data) {
             var articles = data;
             
@@ -26,5 +27,32 @@ module.exports = {
             //if any errors occur the function will keep going and simply return the errors
 
         })
+    },
+    // get = Read CRUD function
+    get: function(query, callback) {
+        Article.find(query)
+        .sort({
+            _id: -1
+        })
+        .exec(function(error, document) {
+            callback(document);
+        });        
+    },
+    //Update CRUD function
+    update: function(query, callback) {
+        Article.update(
+            {
+                _id: query._id
+            },
+            {
+                $set: query
+            },
+            {},
+            callback
+        );
+    },
+    //Delete CRUD function
+    delete: function(query, callback) {
+        Article.remove(query, callback);
     }
 }
