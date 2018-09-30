fed7337b4b83fdfeefba0ab0ad46136bb3a718e9
CodeQuiver <38484031+CodeQuiver@users.noreply.github.com>
2018-09-30 15:00:11 -0400

    further outlining and setup of necessary functions in index,js

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -2,26 +2,52 @@
 // handles what information to display on homepage and calling of routes
 
 $(document).ready(function() {
+// makes sure nothing runs until page has completed loading
+// wraps everything so not indenting formatting here since it would just shift entire page over
+
    
 
-    // ========== FUNCTIONS ========= //
+// ====================== FUNCTIONS ======================== //
+
+// Initialize Page/Reset Page
+function initializePage() {
+    // Empty the article-box div
+    articleBox.empty();
     
-    // Initialize Page/Reset Page
-    function initializePage() {
+    //AJAX request for unsaved articles
+
+        /* if unsaved articles found, render with printArticles function*/
         
-    };
-    // ========== END FUNCTIONS ========= //
+
+        /* else render "no articles" message using printEmpty function */
 
 
-    // ========== MAIN PROGRAM FLOW ========= //
-        // first reference the div where all the articles should be displayed
-        /*  then add event listeners to dynamically generated
-        buttons- both "save article" and "scrape new article"*/
-    
-        // CODE HERE
+};
+// END Initialize Page/Reset Page
 
-        //call initialize page function first thing
-        initializePage();
-    
-    // ========== END MAIN PROGRAM FLOW ========= //
+
+// PRINT ARTICLES to the article-box div
+// END PRINT ARTICLES
+
+
+// PRINT "NO ARTICLES" MESSAGE
+// END PRINT "NO ARTICLES" MESSAGE
+
+
+
+// ====================== END FUNCTIONS  ======================== //
+
+
+// ====================== MAIN PROGRAM FLOW  ======================== //
+    // first reference the div where all the articles should be displayed
+    /*  then add event listeners to dynamically generated
+    buttons- both "save article" and "scrape new article"*/
+    var articleBox = $(".article-box");
+    $(document).on("click", ".btn.save", handleArticleSave);
+    $(document).on("click", ".new-scrape", handleArticleScrape);
+
+    //call initialize page function first thing once page is ready
+    initializePage();
+
+// ====================== END MAIN PROGRAM FLOW  ======================== //
 });
