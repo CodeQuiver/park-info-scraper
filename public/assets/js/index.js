9ac1651d8888d487c0f8cd81753d9bd670e1c8c0
CodeQuiver <38484031+CodeQuiver@users.noreply.github.com>
2018-09-30 15:09:16 -0400

    moved global variables to separate section

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -5,7 +5,13 @@ $(document).ready(function() {
 // makes sure nothing runs until page has completed loading
 // wraps everything so not indenting formatting here since it would just shift entire page over
 
-   
+
+
+// ====================== GLOBAL VARIABLES ======================== //
+var articleBox = $(".article-box");
+// ====================== END GLOBAL VARIABLES ======================== //
+
+
 
 // ====================== FUNCTIONS ======================== //
 
@@ -15,34 +21,43 @@ function initializePage() {
     articleBox.empty();
     
     //AJAX request for unsaved articles
-
-        /* if unsaved articles found, render with printArticles function*/
-        
-
-        /* else render "no articles" message using printEmpty function */
-
-
+    $.get("/api/headlines?saved=false")
+        .then(function(data) {
+            /* if unsaved articles found, render with printArticles function*/
+            if (data && data.length) {
+                printArticles(data);
+            } 
+            /* else render "no articles" message using printEmpty function */
+            else {
+                printEmpty();
+            } 
+        });
 };
 // END Initialize Page/Reset Page
 
 
 // PRINT ARTICLES to the article-box div
+function printArticles(articles) {
+
+};
 // END PRINT ARTICLES
 
 
 // PRINT "NO ARTICLES" MESSAGE
+function printEmpty() {
+    
+};
 // END PRINT "NO ARTICLES" MESSAGE
 
 
-
 // ====================== END FUNCTIONS  ======================== //
+
 
 
 // ====================== MAIN PROGRAM FLOW  ======================== //
     // first reference the div where all the articles should be displayed
     /*  then add event listeners to dynamically generated
     buttons- both "save article" and "scrape new article"*/
-    var articleBox = $(".article-box");
     $(document).on("click", ".btn.save", handleArticleSave);
     $(document).on("click", ".new-scrape", handleArticleScrape);
 
