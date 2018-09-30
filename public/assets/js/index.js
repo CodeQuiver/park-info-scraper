2216f64f4a3e6bdcbd92e311935a1a9f75401908
CodeQuiver <38484031+CodeQuiver@users.noreply.github.com>
2018-09-30 17:10:02 -0400

    completed setup of function for simple individual article listings

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -21,7 +21,7 @@ function initializePage() {
     articleBox.empty();
     
     //AJAX request for unsaved articles
-    $.get("/api/headlines?saved=false")
+    $.get("/api/articles?saved=false")
         .then(function(data) {
             if (data && data.length) {
                 // if unsaved articles found, render with printArticles function
@@ -40,7 +40,15 @@ function initializePage() {
 function printArticles(articles) {
     //function takes in a JSON array containing all articles in database
     // pass each item in array to the "createListing" function
+    var articlesToPrint = [];
 
+    for (var i = 0; i < articles.length; i++) {
+        var currentArticle = articles[i];
+        articlesToPrint.push(createListing(currentArticle));        
+    }
+    // after all the listings have been created and added to the array
+    // append to the articleBox container
+    articleBox.append(articlesToPrint);
 };
 // END PRINT ARTICLES
 
@@ -48,7 +56,25 @@ function printArticles(articles) {
 // CREATE SINGLE ARTICLE LISTING
     // this will be called in printArticles, but writing as standalone because it could be reused
     // this creates a single listing for an article from the scraped data
-    //******* to keep it simple I am starting this using jquery, but I should probably instead create a handlebars object and have handlebars deal with the layout and rendering *****
+    /* to keep it simple I am starting this using jquery, but would be better to
+    create a handlebars object and have handlebars deal with the layout and rendering
+    if I want to use more advanced formatting later */
+    function createListing(currentArticle) {
+        var listing = $(
+            "<h3><a href='" + currentArticle.articleUrl + "'> " + currentArticle.articleTitle + "</a></h3>" +
+            "<p>" + currentArticle.articleSummary+ "<br />" +
+            "<a class='btn btn-success save'> Save Article </a>" +
+            "</p>"
+        );
+
+        //then attach item id to the element
+        listing.data("_id", currentArticle._id);
+        
+        // return the constructed listing
+        return listing;
+    }
+
+
 // END CREATE SINGLE ARTICLE LISTING
 
 
