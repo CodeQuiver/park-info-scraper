f83e57bce01a504cff67e216922e6f88196b1da3
CodeQuiver <38484031+CodeQuiver@users.noreply.github.com>
2018-09-30 14:16:01 -0400

    began setup of index.js client-side javascript for homepage functions, starting with pseudo-coding section divisions and structure

---
+++ public/assets/js/index.js
@@ -0,0 +1,27 @@
+// client-side javascript for homepage functions
+// handles what information to display on homepage and calling of routes
+
+$(document).ready(function() {
+   
+
+    // ========== FUNCTIONS ========= //
+    
+    // Initialize Page/Reset Page
+    function initializePage() {
+        
+    };
+    // ========== END FUNCTIONS ========= //
+
+
+    // ========== MAIN PROGRAM FLOW ========= //
+        // first reference the div where all the articles should be displayed
+        /*  then add event listeners to dynamically generated
+        buttons- both "save article" and "scrape new article"*/
+    
+        // CODE HERE
+
+        //call initialize page function first thing
+        initializePage();
+    
+    // ========== END MAIN PROGRAM FLOW ========= //
+});
